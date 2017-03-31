fbc5921bd1458c4849cbb8001e98c93957c9c75a
DanSutherland1 <dsutherland1@sheffield.ac.uk>
2017-03-31 16:00:21 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+
+$("#card1").flip();
+$("#card2").flip();
+$("#card3").flip();
+$("#card4").flip();
+$("#card5").flip();
+$("#card6").flip();
+$("#card7").flip();
+$("#card8").flip();
+$("#card9").flip();
+$("#card10").flip();
+$("#card11").flip();
+$("#card12").flip();
+$("#card13").flip();
